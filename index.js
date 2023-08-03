025d8c48c5f271048ae8a11f18e5e6b08555611e
BD3849 <bsdinnan@icloud.com>
2023-08-03 05:48:25 -0400

    Set up most of inquirer and began setting up markdown generation

--- index.js
+++ index.js
@@ -1,14 +1,78 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown')
+const generateMarkdown = require('./utils/generateMarkdown.js')
+const fs = require('fs')
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    //license
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+    {
+        type: 'list',
+        message: 'What type of license does this application use?',
+        name: 'license',
+        default: 'MIT',
+        choices: ['MIT'],
+    },
+    {
+        type: 'input',
+        message: 'Provide a description of the application.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Provide installation instructions for the application.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Provide usage information for the application.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Provide contribution guidelines for the application',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'Provide testing instructions for the application',
+        name: 'testing',
+    },
+    ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile(`README.md`, data, (error) => {
+        if(error) {
+            console.error(error)
+        }
+    })
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            const markdwn = generateMarkdown(response)
+        })
+}
+
 
 // Function call to initialize app
 init();
